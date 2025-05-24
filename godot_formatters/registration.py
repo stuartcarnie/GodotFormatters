18fb91fc8f174144823ddcf4e91e16740b70aab7
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-05-24 13:58:07 -0700

    further breaking things up

---
+++ godot_formatters/registration.py
@@ -0,0 +1,326 @@
+# noinspection PyUnresolvedReferences
+from importlib import reload
+import godot_formatters.godot_providers
+
+# we have to force reload the dependent modules to make lldb update the providers if we're re-loading the module
+godot_formatters.godot_providers = reload(godot_formatters.godot_providers)
+from godot_formatters.godot_providers import *
+
+import godot_formatters.godot_types
+
+reload(godot_formatters.godot_types)
+from godot_formatters.godot_types import *
+from godot_formatters.godot_types import (
+    GodotSynthProvider
+)
+
+# fmt: off
+from lldb import (SBCommandReturnObject, SBExecutionContext, SBTypeCategory, eFormatBytes, eFormatCString, eFormatUnicode32, eNoDynamicValues, eDynamicDontRunTarget, eDynamicCanRunTarget, eBasicTypeInvalid, eBasicTypeVoid, eBasicTypeChar, 
+                  eBasicTypeSignedChar, eBasicTypeUnsignedChar, eBasicTypeWChar, eBasicTypeSignedWChar, eBasicTypeUnsignedWChar, eBasicTypeChar16, eBasicTypeChar32, 
+                  eBasicTypeChar8, eBasicTypeShort, eBasicTypeUnsignedShort, eBasicTypeInt, eBasicTypeUnsignedInt, eBasicTypeLong, eBasicTypeUnsignedLong, eBasicTypeLongLong, 
+                  eBasicTypeUnsignedLongLong, eBasicTypeInt128, eBasicTypeUnsignedInt128, eBasicTypeBool, eBasicTypeHalf, eBasicTypeFloat, eBasicTypeDouble, eBasicTypeLongDouble, 
+                  eBasicTypeFloatComplex, eBasicTypeDoubleComplex, eBasicTypeLongDoubleComplex, eBasicTypeObjCID, eBasicTypeObjCClass, eBasicTypeObjCSel, eBasicTypeNullPtr, eReturnStatusSuccessFinishNoResult, eReturnStatusSuccessFinishResult, 
+                  eTypeClassClass, eTypeClassEnumeration, eTypeClassPointer, eTypeOptionCascade)
+from lldb import ( SBValue, SBAddress, SBData, SBType, SBTypeEnumMember, SBTypeEnumMemberList, SBSyntheticValueProvider, SBError, SBTarget, SBDebugger, SBTypeSummary, SBTypeSynthetic, SBTypeNameSpecifier)
+# fmt: on
+
+
+# ********************************************************
+# REGISTRATION
+# ********************************************************
+
+IDRE = "([^,]+)"
+
+
+def id_template(min, max):
+    th = f"{IDRE}"
+    if min == 1 and max == 1:
+        return th
+    temp = f"{th}"
+    for i in range(min - 1):
+        temp += f",{th}"
+    for i in range(max - min):
+        temp += f"(?:,{th})?"
+    return temp
+
+
+def get_template_regex(name: str, min, max) -> str:
+    return f"^(::)?{name}<{id_template(min, max)}>$"
+
+
+# fmt: on
+
+module = sys.modules[__name__]
+cpp_category: SBTypeCategory
+
+
+def attach_synthetic_to_type(type_name, synth_class, is_regex=True):
+    global module, cpp_category
+    # print_trace('attaching synthetic %s to "%s", is_regex=%s' %(synth_class.__name__, type_name, is_regex))
+    synth = SBTypeSynthetic.CreateWithClassName(__name__ + "." + synth_class.__name__)
+    synth.SetOptions(eTypeOptionCascade)
+    cpp_category.AddTypeSynthetic(SBTypeNameSpecifier(type_name, is_regex), synth)
+
+    def summary_fn(valobj, dict):
+        return get_synth_summary(synth_class, valobj, dict)
+
+    # LLDB accesses summary fn's by name, so we need to create a unique one.
+    summary_fn.__name__ = "_get_synth_summary_" + synth_class.__name__
+    setattr(module, summary_fn.__name__, summary_fn)
+    print_trace(f"attaching summary {summary_fn.__name__} to {type_name}, is_regex={is_regex}")
+    summary = SBTypeSummary.CreateWithFunctionName(__name__ + "." + summary_fn.__name__)
+    summary.SetOptions(eTypeOptionCascade)
+    cpp_category.AddTypeSummary(SBTypeNameSpecifier(type_name, is_regex), summary)
+
+    # attach_summary_to_type(summary_fn, type_name, is_regex)
+
+
+def attach_summary_to_type(type_name, real_summary_fn, is_regex=False, real_fn_name: Optional[str] = None):
+    global module, cpp_category
+    if not real_fn_name:
+        real_fn_name = str(real_summary_fn.__qualname__)
+
+    def __spfunc(valobj, dict):
+        try:
+            return real_summary_fn(valobj, dict)
+        except Exception as e:
+            err_msg = "ERROR in " + real_fn_name + ": " + str(e)
+            print_verbose(err_msg)
+            print_verbose(get_exception_trace(e))
+            return f"<{err_msg}>"
+
+    # LLDB accesses summary fn's by name, so we need to create a unique one.
+    __spfunc.__name__ = "__spfunc__" + real_fn_name.replace(".", "_")
+    setattr(module, __spfunc.__name__, __spfunc)
+
+    summary = SBTypeSummary.CreateWithFunctionName(__name__ + "." + __spfunc.__name__)
+    summary.SetOptions(eTypeOptionCascade)
+    cpp_category.AddTypeSummary(SBTypeNameSpecifier(type_name, is_regex), summary)
+
+
+# TODO: Collate globals better
+def clear_globals():
+    GodotSynthProvider.synth_by_id.clear()
+    GodotSynthProvider.next_id = 0
+    hex_color_to_name.clear()
+    global constructed_the_table
+    constructed_the_table = False
+
+
+def register_all_synth_and_summary_providers():
+    force_compat(Opts.MIDEBUGGER_COMPAT)
+    clear_globals()
+    for key in SUMMARY_PROVIDERS:
+        try:
+            attach_summary_to_type(key, SUMMARY_PROVIDERS[key], True)
+        except Exception as e:
+            print_verbose("EXCEPTION: " + str(e))
+    for key in SYNTHETIC_PROVIDERS:
+        try:
+            attach_synthetic_to_type(key, SYNTHETIC_PROVIDERS[key], True)
+        except Exception as e:
+            print_verbose("EXCEPTION st: " + str(e))
+
+
+def monkey_patch_optparse():
+    if not "bool" in optparse.Option.TYPES:
+        optparse.Option.TYPES = optparse.Option.TYPES + ("bool",)
+    if not "bool" in optparse.Option.TYPE_CHECKER:
+        optparse.Option.TYPE_CHECKER["bool"] = lambda option, opt, value: value.lower() in [
+            "true",
+            "t",
+            "yes",
+            "y",
+            "1",
+        ]
+
+
+# ********************************************************
+# COMMANDS
+# ********************************************************
+
+# fmt: off
+from lldb import (SBDebugger, SBCommandReturnObject, SBExecutionContext, eReturnStatusSuccessFinishNoResult, eReturnStatusSuccessFinishResult)
+import optparse
+import shlex
+
+from godot_formatters.options import GodotFormatterOptions, Opts, HELP_STRING_MAP
+class _LLDBCommandBase:
+    program = ""
+    description = ""
+
+    @classmethod
+    def register_lldb_command(cls, debugger: SBDebugger, module_name, container_name):
+        if not cls.program:
+            print("ERROR: No program name specified for command")
+            return
+        try:
+            parser = cls.create_options()
+            cls.__doc__ = parser.format_help() if parser else cls.description
+            # Add any commands contained in this module to LLDB
+            command = "command script add -o -c %s.%s %s %s" % (
+                module_name,
+                cls.__name__,
+                container_name,
+                cls.program,
+            )
+            debugger.HandleCommand(command)
+        except Exception as e:
+            print("ERROR: " + str(e))
+            print('The "{0}" command failed to install.'.format(cls.program))
+            return
+
+        full_program_name = cls.program if not container_name else container_name + " " + cls.program
+        print('The "{0}" command has been installed, type "help {0}" for detailed help.'.format(full_program_name))
+
+    @classmethod
+    def create_options(cls):
+        return None
+
+    def get_short_help(self):
+        return self.description
+
+    def get_long_help(self):
+        return self.description
+
+    def __init__(self, debugger, unused):
+        pass
+
+
+class ReloadCommand(_LLDBCommandBase):
+    program = "reload"
+    description = "Reloads all synthetic types and such."
+
+    def __call__(
+        self,
+        debugger: SBDebugger,
+        command,
+        exe_ctx: SBExecutionContext,
+        result: SBCommandReturnObject,
+    ):
+        # Use the Shell Lexer to properly parse up command options just like a
+        # shell would
+        register_all_synth_and_summary_providers()
+        # not returning anything is akin to returning success
+        return
+
+
+class GetOptsCommand(_LLDBCommandBase):
+    program = "get-opts"
+    description = "This command prints a list of the current option settings for the Godot formatter script."
+
+    def __call__(
+        self,
+        debugger: SBDebugger,
+        command,
+        exe_ctx: SBExecutionContext,
+        result: SBCommandReturnObject,
+    ):
+        # Use the Shell Lexer to properly parse up command options just like a
+        # shell would
+        results = []
+        dir_opts = dir(Opts)
+        for attr in dir(Opts):
+            if attr.startswith("__"):
+                continue
+            result = f"{attr}: {getattr(Opts, attr)}"
+            print(result)
+            results.append(result)
+        result.AppendMessage("\n".join(results))
+        result.SetStatus(eReturnStatusSuccessFinishResult)
+        # not returning anything is akin to returning success
+        return
+
+
+class SetOptsCommand(_LLDBCommandBase):
+    program = "set-opts"
+    description = "This command sets the options for the Godot formatter script."
+
+    @classmethod
+    def create_options(cls):
+        usage = "usage: %prog [options]"
+
+        # Pass add_help_option = False, since this keeps the command in line
+        #  with lldb commands, and we wire up "help command" to work by
+        # providing the long & short help methods below.
+        parser = optparse.OptionParser(
+            description=cls.description,
+            prog=cls.program,
+            usage=usage,
+            add_help_option=False,
+            formatter=optparse.IndentedHelpFormatter(2, 30, width=80),
+        )
+
+        for attr in dir(GodotFormatterOptions):
+            if attr.startswith("__"):
+                continue
+            # get the type of the attribute
+            attr_val = getattr(GodotFormatterOptions, attr)
+            attr_type = type(attr_val)
+            # get the attribute docstring
+            help_string = HELP_STRING_MAP[attr] + f" (default: {attr_val})\n"
+            parser.add_option(
+                f"--{attr.lower().replace('_', '-')}",
+                action="store",
+                type=attr_type.__name__,
+                dest=attr,
+                help=help_string,
+                metavar=f"<{str(attr_type.__name__).upper()}>",
+                # default=attr_val,
+            )
+        # parser.print_help()
+        return parser
+
+    def get_short_help(self):
+        return self.description
+
+    def get_long_help(self):
+        return self.help_string
+
+    def __init__(self, debugger, unused):
+        self.parser = self.create_options()
+        self.help_string = self.parser.format_help()
+
+    def __call__(
+        self,
+        debugger: SBDebugger,
+        command,
+        exe_ctx: SBExecutionContext,
+        result: SBCommandReturnObject,
+    ):
+        # Use the Shell Lexer to properly parse up command options just like a
+        # shell would
+        command_args = shlex.split(command)
+        try:
+            (options, args) = self.parser.parse_args(command_args)
+        except:
+            if command_args and len(command_args) > 0 and ("--help" in command_args or "-h" in command_args):
+                print(self.get_long_help())
+                return
+            # if you don't handle exceptions, passing an incorrect argument to
+            # the OptionParser will cause LLDB to exit (courtesy of OptParse
+            # dealing with argument errors by throwing SystemExit)
+            print("Error: Unknown option")
+            print(self.get_long_help())
+            result.SetError("option parsing failed")
+            return
+
+        option: str
+        set_option = False
+        for option in options.__dict__:
+            if option.startswith("__"):
+                continue
+            if options.__dict__[option] is None:
+                continue
+            set_option = True
+            Opts.__setattr__(option, options.__dict__[option])
+        if not set_option:
+            result.SetError("No options were set")
+            self.parser.print_help()
+            return
+
+        print("Options have been set. Resetting formatters...")
+        register_all_synth_and_summary_providers()
+        result.SetStatus(eReturnStatusSuccessFinishNoResult)
+        # not returning anything is akin to returning success
+        return
